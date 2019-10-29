d7d71c2d95a532d58786d284470283c39140e3d5
Daniil Dvoryanov <daniil.dvoryanov@gmail.com>
2019-10-29 23:55:44 +0300

    fix: various

--- parser.h
+++ parser.h
@@ -4,29 +4,35 @@
 #include <list>
 
 struct Literal {
-    int *integer_value;
-    bool *boolean_value;
-    double *real_value;
+    int *integer_value{};
+    bool *boolean_value{};
+    double *real_value{};
 
-    explicit Literal(int *integer_value = nullptr, bool *boolean_value = nullptr, double *real_value = nullptr) :
+    Literal(int *integer_value, bool *boolean_value, double *real_value) :
             integer_value(integer_value), boolean_value(boolean_value), real_value(real_value) {};
 };
 
 struct Atom {
     char *identifier;
     bool is_keyword;
-    Literal *literal;
+    Literal *literal{};
 
-    Atom(char *identifier, bool is_keyword, Literal *literal = nullptr) :
+    Atom(char *identifier, bool is_keyword, Literal *literal) :
             identifier(identifier), is_keyword(is_keyword), literal(literal) {};
 };
 
 struct Element;
 struct List {
     std::list<Element *> list;
+    bool is_null = false;
 
-    explicit List(Element *element = nullptr) {
-        list.push_back(element);
+    explicit List(Element *element) {
+        if(element != nullptr) {
+            list.push_back(element);
+        }
+        else{
+            is_null = true;
+        }
     }
 
     void addElement(Element *element) {
@@ -34,22 +40,14 @@ struct List {
     }
 };
 
-const List null = List();
+const List null = List(nullptr);
 
 struct Element {
-    Atom *atom;
-    List *list;
+    Atom *atom{};
+    List *list{};
 
-    explicit Element(Atom *atom = nullptr, List *list = nullptr):
+    explicit Element(Atom *atom, List *list):
             atom(atom), list(list){};
-};
-
-struct ElementSequence {
-    std::list <Element *> elementList;
-
-    explicit ElementSequence(Element* element){
-        elementList.push_back(element);
-    }
 };
 
 #endif //F_SIMPLE_PARSER_H
