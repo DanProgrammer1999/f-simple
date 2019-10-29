e6f5373199c8b189f2b96c4e8a4cb5c9fa64b5e6
Daniil Dvoryanov <daniil.dvoryanov@gmail.com>
2019-10-30 00:14:49 +0300

    fix and refactoring

--- parser.h
+++ parser.h
@@ -4,33 +4,36 @@
 #include <list>
 
 struct Literal {
-    int *integer_value{};
-    bool *boolean_value{};
-    double *real_value{};
+    int *integer_value;
+    bool *boolean_value;
+    double *real_value;
 
     Literal(int *integer_value, bool *boolean_value, double *real_value) :
             integer_value(integer_value), boolean_value(boolean_value), real_value(real_value) {};
 };
 
 struct Atom {
-    char *identifier;
-    bool is_keyword;
+    char *identifier{};
+    bool is_keyword = false;
     Literal *literal{};
 
-    Atom(char *identifier, bool is_keyword, Literal *literal) :
-            identifier(identifier), is_keyword(is_keyword), literal(literal) {};
+    Atom(char *identifier, bool is_keyword) :
+            identifier(identifier), is_keyword(is_keyword) {};
+
+    explicit Atom(Literal *literal):
+        literal(literal){};
 };
 
 struct Element;
+
 struct List {
     std::list<Element *> list;
     bool is_null = false;
 
     explicit List(Element *element) {
-        if(element != nullptr) {
+        if (element != nullptr) {
             list.push_back(element);
-        }
-        else{
+        } else {
             is_null = true;
         }
     }
@@ -43,11 +46,11 @@ struct List {
 const List null = List(nullptr);
 
 struct Element {
-    Atom *atom{};
-    List *list{};
+    Atom *atom;
+    List *list;
 
-    explicit Element(Atom *atom, List *list):
-            atom(atom), list(list){};
+    explicit Element(Atom *atom, List *list) :
+            atom(atom), list(list) {};
 };
 
 #endif //F_SIMPLE_PARSER_H
--- parser.y
+++ parser.y
@@ -10,6 +10,7 @@
 	struct Atom *atom;
 	struct List *list;
 	struct Literal *literal;
+	const List *null;
 }
 
 %start Program
@@ -17,36 +18,36 @@
 
 %%
 
-Literal: INTEGER
-	| BOOLEAN
-	| REAL
+Literal: INTEGER	{$$ = new Literal($1, nullptr, nullptr);}
+	| BOOLEAN	{$$ = new Literal(nullptr, $1, nullptr);}
+	| REAL     	{$$ = new Literal(nullptr, nullptr, $1);}
 	;
 
-Atom: IDENTIFIER
-	| KEYWORD
-	| Literal
+Atom: IDENTIFIER	{$$ = new Atom($1, false);}
+	| KEYWORD	{$$ = new Atom($1, true);}
+	| Literal	{$$ = new Atom($1);}
 	;
 
-SimpleList: '(' Element ')'
-	| '(' ')' {$$ = Null}
+SimpleList: '(' Element ')'	{$$ = new List($1);}
+	| '(' ')'		{$$ = null;}
 	;
 
-ElementList: Element
-	| Element ElementList
+ElementList: Element		{$$ = new List($1);}
+	| Element ElementList   {$1->addElement($2); $$ = $1;}
 	;
 
-List: SimpleList
-	| ElementList
+List: SimpleList	{$$ = $1;}
+	| ElementList	{$$ = $1;}
 	;
 
-Element: Atom
-	| List
+Element: Atom	{$$ = new Element($1, nullptr);}
+	| List	{$$ = new Element(nullptr, $1);}
 	;
 
-ElementSequence: Element
-	| Element ElementSequence
+ElementSequence: Element		{$$ = new List($1);}
+	| Element ElementSequence	{$1->addElement($2); $$ = $1;}
 	;
 
-Program: ElementSequence;
+Program: ElementSequence {$$ = $1;};
 
 %%
