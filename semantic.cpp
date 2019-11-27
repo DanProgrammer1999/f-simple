bcb457325eed3ad389b41a43185de81b89a7464a
Daniil Dvoryanov <daniil.dvoryanov@gmail.com>
2019-11-28 00:46:46 +0300

    feat: base for class Function

--- semantic.cpp
+++ semantic.cpp
@@ -3,11 +3,9 @@
 #include "parser.h"
 #include "errors.cpp"
 
-class Function;
-
 class Context;
 
-typedef Element* (*FunctionPointer)(Context *, Elements *);
+typedef Element *(*FunctionPointer)(Context *, List *);
 
 class DefaultFunctions {
 public:
@@ -16,8 +14,8 @@ public:
         std::map<std::string, FunctionPointer> res;
         res["quote"] = quote;
         res["setq"] = setq;
+        res["func"] = func;
 
-//        res["func"] = func;
 //        res["lambda"] = lambda;
 //        res["prog"] = prog;
 //        res["cond"] = cond;
@@ -30,18 +28,18 @@ public:
 
 private:
     // Take one argument and return it as it is
-    static Element* quote(Context *context, Elements *args){
-        if(args->size() > 1){
+    static Element *quote(Context *context, List *args) {
+        if (args->elements.size() > 1) {
             // args number mismatch exception
         }
         // Return the argument itself
-        return args->front();
+        return args->elements.front();
     }
 
     // Take two args (Atom, Element), evaluate second arg,
     // and create (update) entry in the context with name $1 to value $2
-    static Element* setq(Context *context, Elements *args) {
-        if(args->size() > 2){
+    static Element *setq(Context *context, List *args) {
+        if (args->elements.size() > 2) {
             // args number mismatch exception
         }
 
@@ -55,11 +53,51 @@ private:
 
     // Takes three elements (Atom, List, Element): (name, args, body)
     // store args and body, and add a name to the context
-    static Element* func(Context *context, Elements *args){
+    static Element *func(Context *context, List *args) {
 
     };
     // TODO define other functions
 };
+
+class Function : public Element {
+private:
+    std::string name{};
+    std::vector<std::string> *args{};
+    Element *body;
+
+public:
+    Function(Atom *name, List *args, Element *body): body(body) {
+        if(name != nullptr && !name->identifier.empty()){
+            this->name = name->identifier;
+        }
+        // else it is nullptr by default
+
+        for(auto elem: args->elements) {
+            try {
+                Atom *arg = (Atom *) elem;
+                this->args->push_back(arg->identifier);
+            }
+            catch (std::exception &) {
+                // invalid element type exception, must be atom
+            }
+        }
+    }
+
+    Element eval(Context* context, List *args){
+        // if body is literal, simply return its value
+
+        // else if body is atom, try to look it up in context dict, and throw exception if does not exist
+        // but not good approach, since should bind the function to value in context when declared, not when called,
+        // as context may change
+        // better resolve value of literal before creating a function
+
+        // if it's a list, create new context (copy), execute statements one by one, return result.
+    }
+
+    // # TODO maybe?
+    void print() override {}
+};
+
 
 class Context {
 private:
@@ -79,21 +117,21 @@ public:
         return functions;
     }
 
-    bool has(std::string name){
+    bool has(std::string name) {
         auto elem = functions.find(name);
         return elem != functions.end();
     }
 
-    FunctionPointer get(std::string name){
-        if(!this->has(name)){
+    FunctionPointer get(std::string name) {
+        if (!has(name)) {
             return nullptr;
         }
         return functions[name];
     }
 
-    FunctionPointer set(std::string name, FunctionPointer value){
+    FunctionPointer set(std::string name, FunctionPointer value) {
         FunctionPointer res = nullptr;
-        if(has(name)){
+        if (has(name)) {
             res = functions[name];
         }
 
@@ -102,7 +140,7 @@ public:
         return res;
     }
 
-    Context* copy(){
+    Context *copy() {
         auto newContext = new Context();
         // TODO will this make a copy?
         newContext->functions = functions;
